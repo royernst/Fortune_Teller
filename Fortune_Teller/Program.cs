1e1075b2fdfa8239dd18480a3683917dde729354
royernst <roy.e31@hotmail.com>
2017-09-29 15:46:41 -0400

    Project complete and stretch goal accomplished

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -2,41 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using System.Threading.Tasks;
 
 namespace Fortune_Teller
 {
     class Program
     {
+        public static void killSwitch()
+        {
+            Environment.Exit(0);
+        }
         static void Main(string[] args)
         {
             string yrsToRetire = " ";
             string moneyInBank = " ";
             string xportType = " ";
             string vacHome = " ";
+            int userAge = 0;
+            int birthMonth = 0;
+            int numSiblings = 0;
 
-            System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Welcome to Fortune_Teller.exe.");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("\nThis application will predict your future based on several variables.");
+            Console.WriteLine("You can type \"Quit\" at any time to exit the application.");
             System.Threading.Thread.Sleep(2000);
-            Console.WriteLine("This program will tell you your future based on several variables.");
-            System.Threading.Thread.Sleep(3000);
             Console.WriteLine("\nPlease input your first name.  Your input is case-sensitive.");
             string userFirst = Console.ReadLine();
-            //Console.WriteLine(userFirst);
+            if (userFirst.ToLower() == "quit")
+            {
+                killSwitch();
+            }
+            else
+            {
+                //intentionally left blank
+            }
 
-            System.Threading.Thread.Sleep(1000);
-            Console.WriteLine("\nNice to meet you, " + userFirst + ".  I will store that information for future reference.");
+
+            Console.WriteLine("\nNice to meet you, " + userFirst + ".\nI will store that information for future reference.");
             System.Threading.Thread.Sleep(3000);
-            Console.WriteLine("Now, please input your last name.");
+            Console.WriteLine("\nNow, please input your last name.");
             string userLast = Console.ReadLine();
-            //Console.WriteLine(userLast);
+            if (userLast.ToLower() == "quit")
+            {
+                killSwitch();
+            }
+            else
+            {
+                //intentionally left blank
+            }
+            //Takes user first and last names
 
-            System.Threading.Thread.Sleep(1000);
             Console.WriteLine("\nComputing...");
             System.Threading.Thread.Sleep(3000);
             Console.WriteLine("\nNow, please input your age in numerals.");
-            int userAge = int.Parse(Console.ReadLine());
-            //Console.WriteLine(userAge);
+            string ageInput = Console.ReadLine().ToLower();
+            if (ageInput == "quit")
+            {
+                killSwitch();
+            }
+            else
+            {
+                userAge = int.Parse(ageInput);
+            }
+            //Takes userAge, sends through ;
 
             if (userAge < 0)
             {
@@ -58,12 +88,20 @@ namespace Fortune_Teller
             }
             //Console.WriteLine(yrsToRetire + " years to retirement.");
 
-            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("\nInteresting.");
-            System.Threading.Thread.Sleep(3000);
+            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("\nPlease input your birth month in numerals.");
-            int birthMonth = int.Parse(Console.ReadLine());
+            string birthInput = Console.ReadLine().ToLower();
+            if (birthInput == "quit")
+            {
+                killSwitch();
+            }
+            else
+            {
+                birthMonth = int.Parse(birthInput);
+            }
             //Console.WriteLine(birthMonth);
+
 
             if (birthMonth >= 1 && birthMonth < 5)
             {
@@ -87,12 +125,11 @@ namespace Fortune_Teller
                 System.Threading.Thread.Sleep(2000);
                 Console.WriteLine("Program terminating...");
             }
-            //Console.WriteLine("$" + moneyInBank);
+            //another identifier based on birth month
 
-            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("\nMy algorithms are beginning to show a trend line,");
             Console.WriteLine("but I still need more information to make a proper prediction.");
-            System.Threading.Thread.Sleep(3000);
+            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("\nPlease input your favorite ROYGBIV color.  Enter \"Help\" for a list of colors.");
             string favColor = Console.ReadLine().ToLower();
             //Console.WriteLine(favColor);
@@ -125,16 +162,27 @@ namespace Fortune_Teller
                 case "violet":
                     xportType = "Popemobile";
                     break;
+                case "quit":
+                    killSwitch();
+                    break;
                 default:
                     xportType = "Short Bus";
                     break;
             }
 
-            System.Threading.Thread.Sleep(2000);
+            System.Threading.Thread.Sleep(1000);
             Console.WriteLine("\nI feel like I will be able to arrive at a prediction soon.");
             System.Threading.Thread.Sleep(2000);
-            Console.WriteLine("\nFinally, please input the number of siblings you have.  Half- and step- siblings also count.");
-            int numSiblings = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nFinally, please input the number of siblings you have.\nHalf- and step- siblings also count.");
+            string sibInput = Console.ReadLine().ToLower();
+            if (sibInput == "quit")
+            {
+                killSwitch();
+            }
+            else
+            {
+                numSiblings = int.Parse(sibInput);
+            }
             //Console.WriteLine(numSiblings);
 
             if (numSiblings == 0)
@@ -162,21 +210,30 @@ namespace Fortune_Teller
                 vacHome = "Pripyat, Ukraine";
             }
 
-            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("\nVery good.  I have all the information I need.");
-            System.Threading.Thread.Sleep(2000);
+            System.Threading.Thread.Sleep(1000);
             Console.WriteLine("\nCalculating . . .");
             System.Threading.Thread.Sleep(2000);
             Console.WriteLine("Calculating . . .");
             System.Threading.Thread.Sleep(2000);
             Console.WriteLine("Calculating . . .");
             System.Threading.Thread.Sleep(2000);
-            Console.WriteLine("\n\n{0} {1} will retire in {2} years with ${3} in the bank,", userFirst, userLast, yrsToRetire, moneyInBank);
-            Console.WriteLine("a vacation home in {0} and a {1}.", vacHome, xportType);
+            if (yrsToRetire == "1,000,000" && moneyInBank == "0" && vacHome == "Pripyat, Ukraine" && xportType == "Short Bus") 
+                {
+                Console.WriteLine("\n\nSince you are incapable of following instructions: ");
+                Console.WriteLine("{0} {1} will retire in {2} years with ", userFirst, userLast, yrsToRetire);
+                Console.WriteLine("${0} in the bank, a vacation home in {1} and a {2}.", moneyInBank, vacHome, xportType);
+            }
+            else
+            {
+                Console.WriteLine("\n\n{0} {1} will retire in {2} years with ", userFirst, userLast, yrsToRetire);
+                Console.WriteLine("${0} in the bank, a vacation home in {1} and a {2}.", moneyInBank, vacHome, xportType);
+            }
             System.Threading.Thread.Sleep(3000);
             Console.WriteLine("\nI hope you enjoyed your fortune.  Please run again if you want another fortune.");
             System.Threading.Thread.Sleep(3000);
             Console.WriteLine("\nProgram Terminating . . .");
+
         }
     }
 }
